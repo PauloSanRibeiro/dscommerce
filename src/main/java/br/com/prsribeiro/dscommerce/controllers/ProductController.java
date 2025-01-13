81c47c791f87d6efb66de0e1ca06077898dc3e12
PauloSanRibeiro <paulo_pr11@hotmail.com>
2025-01-13 13:47:37 -0300

    Implements DTO, findById

--- src/main/java/br/com/prsribeiro/dscommerce/controllers/ProductController.java
+++ src/main/java/br/com/prsribeiro/dscommerce/controllers/ProductController.java
@@ -1,27 +1,25 @@
 package br.com.prsribeiro.dscommerce.controllers;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.prsribeiro.dscommerce.entities.Product;
-import br.com.prsribeiro.dscommerce.repositories.ProductRepository;
+import br.com.prsribeiro.dscommerce.dto.ProductDTO;
+import br.com.prsribeiro.dscommerce.services.ProductService;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
 	@Autowired
-	private ProductRepository repository;
+	private ProductService service;
 
-	@GetMapping
-	public String getProduct(Long id) {
-		Optional<Product> result = repository.findById(id); // L = long
-		Product product = result.get();
-		return product.getName();
+	@GetMapping(value = "/{id}")
+	public ProductDTO findById(@PathVariable Long id) {
+		return service.findByID(id);
+
 	}
 
 }
--- src/main/java/br/com/prsribeiro/dscommerce/services/ProductService.java
+++ src/main/java/br/com/prsribeiro/dscommerce/services/ProductService.java
@@ -1,5 +1,28 @@
 package br.com.prsribeiro.dscommerce.services;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.prsribeiro.dscommerce.dto.ProductDTO;
+import br.com.prsribeiro.dscommerce.entities.Product;
+import br.com.prsribeiro.dscommerce.repositories.ProductRepository;
+
+@Service
 public class ProductService {
 
+	@Autowired
+	private ProductRepository repository;
+
+	@Transactional(readOnly = true)
+	public ProductDTO findByID(Long id) {
+
+		Optional<Product> result = repository.findById(id);
+		Product product = result.get();
+		ProductDTO dto = new ProductDTO(product);
+
+		return dto;
+	}
 }
